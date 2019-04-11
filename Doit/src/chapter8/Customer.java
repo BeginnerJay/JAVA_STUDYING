5f471069ada1d4e0c691694d935faed82c5b6b6c
JWKim <febukyongira@gmail.com>
2019-04-11 13:59:13 +0900

    commit test

---
+++ Doit/src/chapter8/Customer.java
@@ -0,0 +1,9 @@
+package chapter8;
+
+public class Customer {
+    private int customerID;
+    private String customerName;
+    private String customerGrade;
+    int bonuspoint;
+    double bonusratio;
+}
